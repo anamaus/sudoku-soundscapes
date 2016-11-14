6d8daef62bf5cc2cd44116a65722a8f9325f6c93
Ana <anamil2112@gmail.com>
2016-11-14 10:34:18 +0100

    audio channel class implemented

--- app/js/audioController.js
+++ app/js/audioController.js
@@ -1,53 +1,51 @@
 
+function AudioChannel(channel, sliderId, btnId) {
+    this.channel = channel;
+    var thisChannel = this.channel;
+    $(function () {
+        $(sliderId).slider();
+    });
+    this.slider = $(sliderId);
+    var thatSlider = this.slider;
+    this.slider.slider({
+        min: 0,
+        max: 100,
+        value: 50,
+        range: "min",
+        animate: true,
+        slide: function (event, ui) {
+            thisChannel.setVolume((ui.value) / 100);
+        }
+    });
+    this.btnId = $(btnId);
 
-$(function() {
-    $(".slider").slider();
-});
-var slider = $('.slider');
-slider.slider({
-    min: 0,
-    max: 100,
-    value:50,
-    range: "min",
-    animate: true,
-    slide: function(event, ui) {
-        setVolume((ui.value) / 100);
-    }
-});
+    this.channel.setVolume = function (volume) {
+        thisChannel.volume = volume;
+    };
+    this.channel.setVolume(0.5);
+
+    this.channel.playAudio = function () {
+        if (thisChannel.paused) {
+            thatSlider.fadeIn(1000).css('display', 'inline-block');
+            thisChannel.play();
+            thisChannel.setAttribute('loop', 'loop');
+        } else {
+            thisChannel.pause();
+            thatSlider.css('display', 'none');
+        }
+    };
+    this.btnId.click(function () {
+        thisChannel.playAudio();
 
 
+    });
+}
 
 
 
 // variable to store HTML5 audio element
-var music = document.getElementById('music');
+var channel1 = new AudioChannel(document.getElementById('audio1'), '#slider1', '#btnPlay1' );
+var channel2 = new AudioChannel(document.getElementById('audio2'), '#slider2', '#btnPlay2' );
+var channel3 = new AudioChannel(document.getElementById('audio3'), '#slider3', '#btnPlay3' );
+var channel4 = new AudioChannel(document.getElementById('audio4'), '#slider4', '#btnPlay4' );
 
-function playAudio() {
-    if (music.paused) {
-        slider.fadeIn(1000).css('display','inline-block');
-        music.play();
-        $('#btnPlay').text("pause") ;
-        music.setAttribute('loop', 'loop');
-        setVolume(0.5);
-
-    } else {
-        music.pause();
-        slider.css('display','none');
-        $('#btnPlay').text("play");
-    }
-}
-
-
-$('#btnPlay').click(function () {
-    playAudio();
-
-
-});
-
-
-// function setVolume(volume) {
-//     music.volume = volume;
-// }
-function setVolume(myVolume) {
-    music.volume = myVolume;
-}
