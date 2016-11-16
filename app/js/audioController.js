b1c8076c14e4b6e377f3906a1226fd5050d3ed0f
Ana <anamil2112@gmail.com>
2016-11-16 14:59:43 +0100

    working js library for loop added, needs to be implemented

--- app/js/audioController.js
+++ app/js/audioController.js
@@ -31,10 +31,13 @@ function AudioChannel(channel, sliderId, btnId) {
             thisChannel.setAttribute('loop', 'loop');
         } else {
             thisChannel.pause();
+
             thatSlider.css('display', 'none');
         }
     };
     this.btnId.click(function () {
+
+
         thisChannel.playAudio();
 
 
@@ -48,4 +51,31 @@ var channel1 = new AudioChannel(document.getElementById('audio1'), '#slider1', '
 var channel2 = new AudioChannel(document.getElementById('audio2'), '#slider2', '#btnPlay2' );
 var channel3 = new AudioChannel(document.getElementById('audio3'), '#slider3', '#btnPlay3' );
 var channel4 = new AudioChannel(document.getElementById('audio4'), '#slider4', '#btnPlay4' );
+var channel5 = new AudioChannel(document.getElementById('audio5'), '#slider5', '#btnPlay5' );
+var channel6 = new AudioChannel(document.getElementById('audio6'), '#slider6', '#btnPlay6' );
+var channel7 = new AudioChannel(document.getElementById('audio7'), '#slider7', '#btnPlay7' );
+var channel8 = new AudioChannel(document.getElementById('audio8'), '#slider8', '#btnPlay8' );
+var channel9 = new AudioChannel(document.getElementById('audio9'), '#slider9', '#btnPlay9' );
+var channel10 = new AudioChannel(document.getElementById('audio10'), '#slider10', '#btnPlay10' );
 
+//
+// channel1.channel.addEventListener('timeupdate', function(){
+//     var buffer =.60;
+//     if(this.currentTime > this.duration - buffer){
+//         this.currentTime =5000;
+//         this.play();
+//     }}, false);
+//
+//
+var sound = new Howl({
+    src: ['sounds/ogg/white.ogg'],
+    loop: true,
+    volume: 0.5
+});
+
+
+  //sound.stop();
+
+// $('#btnPlay1').click(function(){
+//     sound.play();
+// })
